370145d16b7ae6da10846f76055ee82fe308f63d
Ori Baram <obrm770@gmail.com>
2021-06-16 15:50:54 +0300

    Added active/not active effect to cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
-const Card = ({ title, image, duration }) => {
+const Card = ({ id, title, image, duration, isActive, onClickHandler }) => {
   return (
-    <div className='card'>
-      <img src={image} alt={title} className='card-img' />
+    <div
+      className={`card ${isActive === null ? null : isActive && 'active'}`}
+      onClick={() => onClickHandler(id)}
+    >
+      <img src={image} alt={title} className='card-img' loading='lazy' />
       <h1 className='card-title'>{title}</h1>
       <div className='card-badge'>
         <i className='far fa-clock'></i> {duration} min
       </div>
+      <div className={isActive !== null && !isActive && 'not-active'}></div>
     </div>
   )
 }
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Card from './Card'
 import cards from '../utils/cards'
 
 const Container = () => {
+  const [clicked, setClicked] = useState(false)
+  const [ID, setID] = useState(null)
+
+  const onClickHandler = (id) => {
+    setClicked(true)
+    setID(id)
+  }
+
   return (
     <section className='container'>
-      <h2>Choose a short workout</h2>
+      <h2>{clicked ? `Let's Go` : `Choose a short workout`}</h2>
       <div className='cards-container'>
         {cards.map((card) => {
-          const { id, title, image, duration } = card
           return (
-            <Card id={id} title={title} image={image} duration={duration} />
+            <Card
+              id={card.id}
+              {...card}
+              isActive={
+                clicked && ID === card.id
+                  ? true
+                  : clicked && ID !== card.id
+                  ? false
+                  : null
+              }
+              onClickHandler={onClickHandler}
+            />
           )
         })}
       </div>
